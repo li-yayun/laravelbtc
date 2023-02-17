1d576d7a2a3619da25d05fa466f7dc0dd1e9b43a
lyy <lyy6625756>
2023-02-17 17:26:56 +0800

    JWT demo

--- btc/app/Http/Controllers/LoginController.php
+++ btc/app/Http/Controllers/LoginController.php
@@ -4,14 +4,29 @@
 namespace App\Http\Controllers;
 
 
+use App\Models\User;
 use Illuminate\Http\Request;
-use Tymon\JWTAuth\JWT;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Tymon\JWTAuth\JWTAuth;
 
 class LoginController extends Controller
 {
 
-    public function loginByJwt(Request $request){
-        $credentials = request(['email', 'password']);
-        auth('api')->attempt($credentials);
+    public function loginByJwt(Request $request): \Illuminate\Http\JsonResponse
+    {
+        $condition = \request(['tel','password']);
+        $token = auth('api')->attempt($condition);
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'expires_in' => auth('api')->factory()->getTTL() * 60
+        ]);
+    }
+
+
+    public function getUserDetail(Request $request){
+        $user = Auth::user();
+        dd($user);
     }
 }
--- btc/routes/api.php
+++ btc/routes/api.php
@@ -14,8 +14,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+
+Route::middleware('api:auth')->get('/getUser', [\App\Http\Controllers\LoginController::class,'getUserDetail']);
+
+/**
+ * 需要接口登录的
+ */
+Route::middleware('auth:api')->group(function () {
+    Route::get('/getUser',  [\App\Http\Controllers\LoginController::class,'getUserDetail']);
 });
+
+/**
+ * 登录接口
+ */
+Route::post('/login',[\App\Http\Controllers\LoginController::class,'loginJwt']);
+
+
 
 
